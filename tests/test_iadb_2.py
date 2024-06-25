fdd8614eedb4aa453be8f7299c067438afbd96a0
Anton Schreiner <antonschrein@gmail.com>
2024-06-25 16:20:41 +0200

    upd diff model test

--- tests/test_iadb_2.py
+++ tests/test_iadb_2.py
@@ -33,19 +33,19 @@ class SmolActivation(torch.nn.Module):
 
 class SmolBlock(torch.nn.Module):
 
-    def __init__(self, out_channels, in_channels, stride, kernel_size=3, padding=1) -> None:
+    def __init__(self, out_channels, in_channels, stride, kernel_size=3, padding=1, dilation=1) -> None:
         super().__init__()
 
         interim_features = in_channels
 
         self.conv1          = torch.nn.Conv2d(in_channels=in_channels, out_channels=interim_features,
-                                              kernel_size=kernel_size, stride=stride, padding=padding)
+                                              kernel_size=kernel_size, stride=stride, padding=padding, dilation=stride, groups=1)
         self.dwconv         = torch.nn.Conv2d(in_channels=interim_features + in_channels,
                                               out_channels=out_channels, kernel_size=1, stride=1, padding=0, groups=out_channels)
-        self.spline_degree  = 4
-        self.spline_k       = torch.nn.Parameter(torch.randn(self.spline_degree, 1, out_channels, 1, 1), requires_grad=True)
+        # self.spline_degree  = 4
+        # self.spline_k       = torch.nn.Parameter(torch.randn(self.spline_degree, 1, out_channels, 1, 1), requires_grad=True)
         self.activation     = torch.nn.GELU()
-        # self.activation     = SmolActivation()
+        self.activation2    = SmolActivation()
 
     def forward(self, i):
         x = self.conv1(i)
@@ -53,7 +53,7 @@ class SmolBlock(torch.nn.Module):
         x = self.dwconv(torch.cat([i, x], dim=1))
         # apply spline
         x = self.activation(x)
-        # x = self.spline_k[0] + x * self.spline_k[1] + x**2 * self.spline_k[2] + x**3 * self.spline_k[3]
+        # x = x * self.spline_k[0] / 1.0 + self.activation2(x) * self.spline_k[1] / 1.0
         return x
 
 class SmolUnet(torch.nn.Module):
@@ -66,14 +66,20 @@ class SmolUnet(torch.nn.Module):
         self.num_features_1 = 16
         self.feature_extraction_conv    = torch.nn.Conv2d(in_channels=self.num_features_0,
                                                           out_channels=self.num_features_1, kernel_size=3, stride=1, padding=1) 
-        self.smol_block_down_1          = SmolBlock(out_channels=32, in_channels=self.num_features_1, kernel_size=3, stride=1)
-        self.smol_block_down_2          = SmolBlock(out_channels=64, in_channels=32, kernel_size=3, stride=1)
-        self.smol_block_down_3          = SmolBlock(out_channels=128, in_channels=64, kernel_size=3, stride=1)
+        
+        self.smol_block_down_1          = SmolBlock(out_channels=32,    in_channels=self.num_features_1,    kernel_size=3, stride=1, dilation=16)
+        self.smol_block_down_2          = SmolBlock(out_channels=64,    in_channels=32,                     kernel_size=3, stride=1, dilation=4)
+        self.smol_block_down_3          = SmolBlock(out_channels=128,   in_channels=64,                     kernel_size=3, stride=1, dilation=2)
+        
         self.smol_bottleneck            = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.smol_block_up_1            = SmolBlock(out_channels=64, in_channels=128, kernel_size=3, stride=1)
-        self.smol_block_up_2            = SmolBlock(out_channels=32, in_channels=64, kernel_size=3, stride=1)
-        self.smol_block_up_3            = SmolBlock(out_channels=16, in_channels=32, kernel_size=3, stride=1)
-        self.feature_reconstruction_conv = torch.nn.Conv2d(in_channels=self.num_features_1, out_channels=self.num_output_channels, kernel_size=3, stride=1, padding=1)
+        self.smol_bottleneck_dw         = torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1, stride=1, padding=0)
+        
+        self.smol_block_up_1            = SmolBlock(out_channels=64,    in_channels=128,    kernel_size=3, stride=1)
+        self.smol_block_up_2            = SmolBlock(out_channels=32,    in_channels=64,     kernel_size=3, stride=1)
+        self.smol_block_up_3            = SmolBlock(out_channels=16,    in_channels=32,     kernel_size=3, stride=1)
+
+        self.feature_reconstruction_conv = torch.nn.Conv2d(in_channels=self.num_features_1,
+                                                            out_channels=self.num_output_channels, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x, alpha):
         N, C, H, W = x.shape
@@ -96,13 +102,17 @@ class SmolUnet(torch.nn.Module):
         # print(f"x3.shape={x3.shape}")
 
         _x4 = self.smol_bottleneck(_x3)
-        x4 = torch.nn.Upsample(scale_factor=2, mode='bilinear')(_x4)
+        _x41 = torch.nn.GELU()(_x4)
+        _x42 = self.smol_bottleneck_dw(_x41)
+        _x43 = torch.nn.GELU()(_x42)
+        x4 = torch.nn.Upsample(scale_factor=2, mode='bilinear')(_x43)
         x5 = self.smol_block_up_1(x3 + x4)
         x6 = torch.nn.Upsample(scale_factor=2, mode='bilinear')(x5)
         x7 = self.smol_block_up_2(x2 + x6)
         x8 = torch.nn.Upsample(scale_factor=2, mode='bilinear')(x7)
         x9 = self.smol_block_up_3(x1 + x8)
-        x10 = self.feature_reconstruction_conv(x9)
+        x10 = self.feature_reconstruction_conv(x0 + x9)
+        x11 = torch.nn.Sigmoid()(x10)
         return x10
 
 
@@ -114,10 +124,9 @@ def sample_iadb(model, x0, nb_step):
     x_alpha = x0
     for t in range(nb_step):
         alpha_start = (t/nb_step)
-        alpha_end =((t+1)/nb_step)
-
-        d = model(x_alpha, torch.tensor([alpha_start], device=x_alpha.device))
-        x_alpha = x_alpha + (alpha_end-alpha_start)*d
+        alpha_end   = ((t+1)/nb_step)
+        d           = model(x_alpha, torch.tensor([alpha_start], device=x_alpha.device))
+        x_alpha     = x_alpha + (alpha_end - alpha_start) * d
 
     return x_alpha
 
@@ -126,7 +135,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Load cats
 # https://www.kaggle.com/datasets/spandan2/cats-faces-64x64-for-generative-models
 size            = 64
-batch_size      = 8
+batch_size      = 16
 cat_tensors     = []
 cur_batch       = []
 MAX_NUM_CATS    = 1 << 10
